2fc2c87b492681776a37365aa02fdcbd05d86cf9
Jose Roberto Garcia Leon <jose.garcia@infotec.mx>
2018-05-14 18:24:31 -0500

    API Openweather

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+
+
+use \GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Response;
+
+$api_key = getenv('OPEN_WEATHER_API');
+
+$app = new Silex\Application();
+$app->get('/clima', function() use($app,$api_key) {
+    
+	$client = new Client();
+	$url = "http://api.openweathermap.org/data/2.5/weather?id=3530597&appid=$api_key&units=metric";
+	$response = $client->get($url);
+    $body = $response->getBody();
+	
+	return new Response($body,200,array('Conten-Type' => 'aplication/json'));
+});
+$app->run();
